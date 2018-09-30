bdfa176225a2686f22b5a227a2c62a7bffb23aa6
mjv761 <michael.vanderkamp@usask.ca>
2018-09-30 12:31:23 -0600

    Add test suite for PointerData

---
+++ test/PointerData.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @file PointerData.js
+ *
+ * Test suite for the PointerData class.
+ */
+
+const PointerData = require('../src/PointerData.js');
+
+describe('constructor(event, identifier)', () => {
+  test('placeholder', () => {
+    expect(true).toBe(true);
+  });
+});
+
