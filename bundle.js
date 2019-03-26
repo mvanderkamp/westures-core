1dac0e961cbded6a15e8c80a7aecf7805fedf757
mjv761 <michael.vanderkamp@usask.ca>
2019-03-25 21:59:13 -0600

    Update docs and bundle to version 0.6.0

--- bundle.js
+++ bundle.js
@@ -965,9 +965,6 @@ const smooth = Symbol('smooth');
  */
 const Smoothable = (superclass) => class Smoothable extends superclass {
   /**
-   * @constructor
-   * @memberof module:westures-core.Smoothable
-   *
    * @param {string} name - The name of the gesture.
    * @param {Object} [options]
    * @param {boolean} [options.smoothing=true] Whether to apply smoothing to
@@ -1000,6 +997,9 @@ const Smoothable = (superclass) => class Smoothable extends superclass {
 
   /**
    * Restart the Smoothable gesture.
+   *
+   * @private
+   * @memberof module:westures-core.Smoothable
    */
   restart() {
     this[stagedEmit] = null;
@@ -1009,6 +1009,8 @@ const Smoothable = (superclass) => class Smoothable extends superclass {
    * Smooth out the outgoing data.
    *
    * @private
+   * @memberof module:westures-core.Smoothable
+   *
    * @param {object} next - The next batch of data to emit.
    * @param {string] field - The field to which smoothing should be applied.
    *
@@ -1032,6 +1034,8 @@ const Smoothable = (superclass) => class Smoothable extends superclass {
    * Average out two values, as part of the smoothing algorithm.
    *
    * @private
+   * @memberof module:westures-core.Smoothable
+   *
    * @param {number} a
    * @param {number} b
    *
