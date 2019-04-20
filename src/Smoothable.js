ff33eed15258f978c52f0b01542810e41dc52d02
mjv761 <michael.vanderkamp@usask.ca>
2019-04-19 22:14:01 -0600

    Check pointer type before applying smoothing
    Use the window.matchMedia() programmatic access to the CSS @media query
    for (pointer: coarse) to determine whether smoothing is necessary.
    Only done once when the gesture is instantiated- no subsequent runtime
    cost.

--- src/Smoothable.js
+++ src/Smoothable.js
@@ -8,6 +8,35 @@ const cascade = Symbol('cascade');
 const smooth = Symbol('smooth');
 
 /**
+ * Determines whether to apply smoothing. Smoothing is on by default but turned
+ * off if either:
+ *  1. The user explicitly requests that it be turned off.
+ *  2. The active poiner is not "coarse".
+ *
+ * @see {@link
+ * https://developer.mozilla.org/en-US/docs/Web/API/Window/matchMedia}
+ *
+ * @private
+ * @inner
+ * @memberof westures-core.Smoothable
+ *
+ * @param {boolean} isRequested - Whether smoothing was requested by the user.
+ *
+ * @returns {boolean} Whether to apply smoothing.
+ */
+function smoothingIsApplicable(isRequested = true) {
+  if (isRequested) {
+    try {
+      return window.matchMedia('(pointer: coarse)').matches;
+    } catch (e) {
+      console.warn(e);
+    }
+    return true;
+  }
+  return false;
+}
+
+/**
  * A Smoothable gesture is one that emits on 'move' events. It provides a
  * 'smoothing' option through its constructor, and will apply smoothing before
  * emitting. There will be a tiny, ~1/60th of a second delay to emits, as well
@@ -42,10 +71,10 @@ const Smoothable = (superclass) => class Smoothable extends superclass {
      * @param {object} data - The data to emit.
      */
     this.smooth = null;
-    if (options.hasOwnProperty('smoothing') && !options.smoothing) {
-      this.smooth = data => data;
-    } else {
+    if (smoothingIsApplicable(options.smoothing)) {
       this.smooth = this[smooth].bind(this);
+    } else {
+      this.smooth = data => data;
     }
 
     /**
