f2764c46e21180389991cff510f689e3e9d831ee
mjv761 <michael.vanderkamp@usask.ca>
2019-08-25 14:41:02 -0600

    Export all constants, utilities, and classes

--- index.js
+++ index.js
@@ -9,14 +9,24 @@
 'use strict';
 
 const Gesture = require('./src/Gesture.js');
+const Input = require('./src/Input.js');
 const Point2D = require('./src/Point2D.js');
+const PointerData = require('./src/PointerData.js');
 const Region = require('./src/Region.js');
 const Smoothable = require('./src/Smoothable.js');
+const State = require('./src/State.js');
+const constants = require('./src/constants.js');
+const utils = require('./src/utils.js');
 
 module.exports = {
   Gesture,
+  Input,
   Point2D,
+  PointerData,
   Region,
   Smoothable,
+  State,
+  ...constants,
+  ...utils,
 };
 
